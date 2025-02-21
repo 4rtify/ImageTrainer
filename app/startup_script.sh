5c6647d48b1f7f3c745dd911d1c8a533426c913b
Nikolay Miroshnichenko <80424809+4rtify@users.noreply.github.com>
2025-02-21 12:01:46 +1100

    fixing changes

--- app/startup_script.sh
+++ app/startup_script.sh
@@ -1,3 +1,3 @@
 python app/download_script.py crlab redi/ YOLO/data/
-python YOLO/yolo/lazy.py task=train dataset=butt task.data.batch_size=8 model=v9-s
+python YOLO/yolo/lazy.py task=train dataset=butt cpu_nums=12 task.data.batch_size=8 model=v9-s
  weight=False

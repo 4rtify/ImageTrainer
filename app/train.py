aeecdfa2a98b9f2cb9934e3fb00587f0a459c197
Nikolay Miroshnichenko <80424809+4rtify@users.noreply.github.com>
2025-03-05 10:55:58 +1100

    added path changes

--- app/train.py
+++ app/train.py
@@ -4,9 +4,9 @@ from pathlib import Path
 import hydra
 from lightning import Trainer
 
-project_root = Path(__file__).resolve().parent
-print(project_root)
-sys.path.append(str(project_root))
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append("/home/user/ImageTrainer/YOLO")
+print(sys.path)
 
 from yolo.config.config import Config
 from yolo.tools.solver import InferenceModel, TrainModel, ValidateModel
